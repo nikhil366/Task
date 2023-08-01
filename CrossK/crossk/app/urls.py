c3929b2c77ea8641e84bbc30371852a376dd2479
NikhilUpadhyay <54943207+nikhil366@users.noreply.github.com>
2023-08-01 14:11:20 +0530

    Delete CrossK directory

--- CrossK/crossk/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from app import views
-
-urlpatterns = [
-    path('category/', views.CategoryView.as_view(), name='category'),
-    path('item/', views.ItemViewSet.as_view(), name='itemview'),
-    path('price/', views.ItemPriceViewSet.as_view(), name='item_price')
-    
-]
